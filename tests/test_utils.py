f01d0edfc2b06c62a7f4d35cc496acac391fe30d
ZaneBaker2001 <110494531+ZaneBaker2001@users.noreply.github.com>
2025-08-12 11:47:37 -0500

    Add files via upload

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,1 +1,10 @@
+from genlab.utils import set_seed, stable_hash
 
+def test_seed_repeatability():
+    a = set_seed(1234)
+    b = set_seed(1234)
+    assert a == b
+
+def test_hash():
+    assert stable_hash("abc") == stable_hash("abc")
+    assert stable_hash("abc") != stable_hash("abd")
